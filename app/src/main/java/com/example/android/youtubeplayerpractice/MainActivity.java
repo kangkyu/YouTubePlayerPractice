9feff107bb9076111a1c0b59f521828437480e32
Kang-Kyu Lee <kangkyu1111@gmail.com>
2018-07-10 22:48:22 -0700

    cueVideo a video (put DeveloperKey class in gitignore file)

--- app/src/main/java/com/example/android/youtubeplayerpractice/MainActivity.java
+++ app/src/main/java/com/example/android/youtubeplayerpractice/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.android.youtubeplayerpractice;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+
+    YouTubePlayerView youTubeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+        youTubeView.initialize(DeveloperKey.GOOGLE_API_KEY, this);
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean wasRestored) {
+
+        if (!wasRestored) {
+            youTubePlayer.cueVideo("wKJ9KzGQq0w");
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+        String errorMessage = String.format(getString(R.string.error_player), youTubeInitializationResult.toString());
+        Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
     }
 }
